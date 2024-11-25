a2ad5faec6bd3519a49a9676c6099f52bdbb4df1
aslam zaman <aslamcmes@gmail.com>
2024-11-26 00:25:47 +0600

    sales report updated with react-to-print

--- app/salereport/page.js
+++ app/salereport/page.js
@@ -5,9 +5,7 @@ import { formatedDate, formatedDateDot, numberWithComma, sortArray } from "@/lib
 require("@/lib/fonts/Poppins-Bold-normal");
 require("@/lib/fonts/Poppins-Regular-normal");
 import { getDataFromFirebase } from "@/lib/firebaseFunction";
-import jsPDF from "jspdf";
-import { jsPDFPrintFirstPage, jsPDFPrintOtherPage } from "@/lib/JspdfPrintPage";
-
+import PrintPage from "@/components/salereport/PrintPage";
 
 
 const Salereport = () => {
@@ -99,104 +97,6 @@ const Salereport = () => {
     }
 
 
-    // Print handler
-    const printMultiplePageHandler = () => {
-        const doc = new jsPDF({
-            orientation: 'p',    // Portrait orientation
-            unit: 'mm',          // Unit of measurement in millimeters
-            format: 'a4',        // A4 paper size
-            putOnlyUsedFonts: true,
-            floatPrecision: 16   // High precision for measurements
-        });
-
-
-        const salesNormalize = sales.map(sale => {
-            const dt = formatedDateDot(sale.dt, true);
-            const wgt = parseFloat(sale.weight).toFixed(2);
-            const rte = parseFloat(sale.rate).toFixed(2);
-            const rate = `${wgt}@${rte}`;
-            const total = numberWithComma(sale.total, true);
-            const nm = sale.customer;
-            const itm = sale.item;
-            const customer = nm.length >= 25 ? nm.substring(0, 23) + "..." : nm;
-            const item = itm.length >= 15 ? itm.substring(0, 15) + "..." : itm;
-            return {
-                ...sale,
-                customer, item, dt, rate, total
-            }
-        })
-
-
-        const withTotal = [...salesNormalize, { dt: "", customer: 'Total', item: "", shipment: "", rate: "", total: numberWithComma(totalTaka, true) }];
-
-
-        const dataFormat = [
-            {
-                head: 'Date',
-                fld: 'dt',
-                pos: 26,
-                aln: 'center'
-            },
-            {
-                head: 'Customer',
-                fld: 'customer',
-                pos: 43,
-                aln: 'left'
-            },
-            {
-                head: 'Item',
-                fld: 'item',
-                pos: 100,
-                aln: 'left'
-            },
-            {
-                head: 'Ship.',
-                fld: 'shipment',
-                pos: 135,
-                aln: 'center'
-            },
-            {
-                head: 'Rate',
-                fld: 'rate',
-                pos: 155,
-                aln: 'center'
-            },
-            {
-                head: 'Total',
-                fld: 'total',
-                pos: 194,
-                aln: 'right'
-            }
-        ]
-
-        doc.setFontSize(16);
-        doc.setFont("times", "bold");
-        doc.text("SALES REPORT", 105, 15, 'center');
-        doc.setFontSize(10);
-        doc.setFont("times", "normal");
-        doc.text(`Period: ${formatedDateDot(dt1)}-${formatedDateDot(dt2)}`, 105, 20, 'center');
-        doc.text(`Print Date: ${formatedDateDot(new Date())}`, 194, 30, 'right');
-
-
-
-        const firstPageData = withTotal.slice(0, 49);  // First 20 rows
-        const otherPageData = withTotal.slice(49);     // Remaining rows
-
-        // Print the first page
-        jsPDFPrintFirstPage({ doc }, firstPageData, dataFormat, 35);
-        // Add a new page and print subsequent pages
-        if (otherPageData.length > 0) {
-            doc.addPage();
-            jsPDFPrintOtherPage({ doc }, otherPageData, dataFormat, 15, 54);
-        }
-
-        // Save the PDF with a timestamped filename
-        doc.save(`${new Date().toISOString()}.pdf`);
-    };
-
-
-
-
 
     return (
         <>
@@ -213,9 +113,8 @@ const Salereport = () => {
                     <input onChange={e => setDt2(e.target.value)} value={dt2} type="date" id='dt2' name="dt2" required className="w-[155px] px-4 py-1.5 text-gray-600 ring-1 focus:ring-4 ring-blue-300 outline-none rounded duration-300" />
                     <button onClick={searchClickHandler} className="text-center mx-0.5 px-4 py-2 bg-green-600 hover:bg-green-800 text-white font-semibold rounded-md focus:ring-1 ring-blue-200 ring-offset-2 duration-300  cursor-pointer">Search</button>
                     <button onClick={refreshClickHandler} className="text-center mx-0.5 px-4 py-2 bg-violet-600 hover:bg-violet-800 text-white font-semibold rounded-md focus:ring-1 ring-blue-200 ring-offset-2 duration-300  cursor-pointer">Refresh</button>
-                    <button onClick={printMultiplePageHandler} className="text-center mx-0.5 px-4 py-2 bg-gray-600 hover:bg-gray-800 text-white font-semibold rounded-md focus:ring-1 ring-blue-200 ring-offset-2 duration-300  cursor-pointer">Print</button>
-
-
+                  
+                    <PrintPage data={{sales,totalTaka}} />
                 </div>
                 <table className="w-full border border-gray-200">
                     <thead>
---
+++ components/salereport/PrintPage.js
@@ -0,0 +1,141 @@
+
+// npm install react-to-print@3.0.2
+import React, { useState, useRef, useCallback } from "react";
+import { useReactToPrint } from "react-to-print";
+import { formatedDateDot, numberWithComma } from "@/lib/utils";
+
+
+
+const PrintPage = ({ data }) => {
+    const [show, setShow] = useState(false);
+    const componentRef = useRef(null);
+
+    const showPrintForm = async () => {
+        setShow(true);
+        console.log(data)
+    }
+
+    const closePrintForm = () => {
+        setShow(false);
+    }
+
+    const pageStyle = `@media print {
+            @page {
+                size: A4 portrait;
+                margin: 0.5in;
+            }
+            footer{
+                page-break-after: always;
+            }    
+            #noPrint{
+                display:none;
+            }  
+            #page{
+                font-size: 12px;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+        }`;
+
+
+    const printFn = useReactToPrint({
+        contentRef: componentRef,
+        pageStyle: pageStyle,
+        documentTitle: `${new Date().toISOString()}_Invoice`,
+    });
+
+    const printHandler = useCallback(() => {
+        printFn();
+    }, [printFn]);
+
+    return (
+        <>
+            {show && (
+                <div className="fixed inset-0 py-16 bg-black bg-opacity-30 backdrop-blur-sm z-10 overflow-auto">
+                    <div className="w-[794px] mx-auto mb-10 bg-white border-2 border-gray-300 rounded-md shadow-md duration-300">
+                        <div className="px-6 md:px-6 py-2 flex justify-between items-center border-b border-gray-300">
+
+                            <h1 className="text-xl font-bold text-blue-600">Print Process Form</h1>
+                            <div className="w-auto flex items-center space-x-4">
+                                <button onClick={printHandler} className="w-8 h-8 p-0.5 bg-gray-50 hover:bg-gray-300 rounded-md ring-1 ring-gray-300">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-full h-full p-[2px] stroke-black">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
+                                    </svg>
+                                </button>
+
+                                <button onClick={closePrintForm} className="w-8 h-8 p-0.5 bg-gray-50 hover:bg-gray-300 rounded-md ring-1 ring-gray-30">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-full h-full stroke-black">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+
+                        </div>
+                        <div className="w-full h-auto p-16">
+                            <div ref={componentRef} className="w-full h-full text-black">
+                                <h1 className="mt-[30px] text-center font-bold uppercase text-[20px]">SALES REPOR</h1>
+                                <p className="text-center">Date: {formatedDateDot(new Date(), true)}</p>
+
+                                <br />
+
+                                <table className="w-full text-[12px] border border-gray-200">
+                                    <thead>
+                                        <tr className="w-full bg-gray-200">
+                                            <th className="text-center border-b border-gray-200 px-4 py-2">Date</th>
+                                            <th className="text-start border-b border-gray-200 px-4 py-2">Customer</th>
+                                            <th className="text-start border-b border-gray-200 px-4 py-2">Item</th>
+                                            <th className="text-center border-b border-gray-200 px-4 py-2">Shipment</th>
+                                            <th className="text-end border-b border-gray-200 px-4 py-2">Weight</th>
+                                            <th className="text-end border-b border-gray-200 px-4 py-2">Rate</th>
+                                            <th className="text-end border-b border-gray-200 px-4 py-2">Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data.sales.length ? (
+                                            data.sales.map((sale, i) => (
+                                                <tr className="border-b border-gray-200 hover:bg-gray-100" key={i}>
+                                                    <td className="text-center py-0.5 px-4">{formatedDateDot(sale.dt, true)}</td>
+                                                    <td className="text-start py-0.5 px-4">{sale.customer}</td>
+                                                    <td className="text-start py-0.5 px-4">{sale.item}</td>
+                                                    <td className="text-center py-0.5 px-4">{sale.shipment}</td>
+                                                    <td className="text-end py-0.5 px-4">{numberWithComma(sale.weight, true)}</td>
+                                                    <td className="text-end py-0.5 px-4">{numberWithComma(sale.rate, true)}</td>
+                                                    <td className="text-end py-0.5 px-4">{numberWithComma(sale.total, true)}</td>
+                                                </tr>
+                                            ))
+                                        ) : (
+                                            <tr>
+                                                <td colSpan={7} className="text-center py-10 px-4">
+                                                    Data not available.
+                                                </td>
+                                            </tr>
+                                        )}
+
+                                        <tr className="font-bold border-b border-gray-200 hover:bg-gray-100">
+                                            <td className="text-center py-0.5 px-4"></td>
+                                            <td className="text-start py-0.5 px-4">Total</td>
+                                            <td className="text-center py-0.5 px-4"></td>
+                                            <td className="text-center py-0.5 px-4"></td>
+                                            <td className="text-center py-0.5 px-4"></td>
+                                            <td className="text-center py-0.5 px-4"></td>
+                                            <td className="text-end py-0.5 px-4">{numberWithComma(data.totalTaka, true)}</td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+
+                                <br />
+                                <br />
+                                <p><span className="font-bold">Note: </span>Thank you for choosing us</p>
+
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            )}
+            <button onClick={showPrintForm} className="text-center mx-0.5 px-4 py-2 bg-gray-600 hover:bg-gray-800 text-white font-semibold rounded-md focus:ring-1 ring-blue-200 ring-offset-2 duration-300  cursor-pointer">Print Form</button>
+        </>
+    )
+}
+export default PrintPage;
